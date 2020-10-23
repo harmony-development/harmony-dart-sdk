524d27131b859ddcdb1c16eb93da5afcc976d7da
Alexey Peschany <nelex111@gmail.com>
2020-10-23 15:01:58 +0300

    Update tests

--- bin/harmony_test.dart
+++ bin/harmony_test.dart
@@ -1,56 +1,63 @@
+import 'dart:io';
+
 import 'package:harmony_sdk/harmony.dart' as harmony;
+
 main() async {
   var stopwatch = new Stopwatch()..start();
 
-  var client = harmony.Client(harmony.Homeserver("janpontaoski.ddns.net"));
+  var client = harmony.Homeserver("localhost");
   print("client created in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  var ok = await client.login("eee@eee.eee", "1Oopooo");
-  if (!ok) {
-    throw ok;
-  }
+  await client.login("eee@eee.eee", "1Oopooo");
   print("client logged in in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  ok = await client.federate(harmony.Homeserver("localhost"));
-  print("client federated in ${stopwatch.elapsed.toString()}, succeeded: ${ok}");
+  var guild = await client.createGuild("club");
+  print("guild created in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  var guilds = await client.joinedGuilds();
+  print("${(await client.joinedGuilds()).length} joined guilds");
   print("guilds listed in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  var channel = await guilds[0].createChannel("yeet");
+  var channel = await guild.createChannel("yeet");
   print("channel created in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  var message = await channel.sendMessage(content: "yeet");
+  var message = await channel.sendMessage("yeet");
   print("message sent in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  message.edit(content: "yaoi");
-  print("message edited in ${stopwatch.elapsed.toString()}");
-  stopwatch.reset();
+  // message.edit("yaoi");
+  // print("message edited in ${stopwatch.elapsed.toString()}");
+  // stopwatch.reset();
 
-  message.delete();
-  print("message deleted in ${stopwatch.elapsed.toString()}");
-  stopwatch.reset();
+  // message.delete();
+  // print("message deleted in ${stopwatch.elapsed.toString()}");
+  // stopwatch.reset();
 
   await channel.delete();
   print("channel deleted in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  var invite = await guilds[0].createInvite("yaoi");
+  var invite = await guild.createInvite("yaoi");
   print("invite created in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
-  await guilds[0].refresh();
-  print("${(await guilds[0].invites).length} invites");
+  await guild.refresh();
+  print("${(await guild.invites).length} invites");
   print("invites listed in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
 
   await invite.delete();
   print("invite deleted in ${stopwatch.elapsed.toString()}");
   stopwatch.reset();
+
+  await guild.delete();
+  print("guild deleted in ${stopwatch.elapsed.toString()}");
+  stopwatch.reset();
+
+  print("seems like everything works, good job!");
+  exit(0);
 }
