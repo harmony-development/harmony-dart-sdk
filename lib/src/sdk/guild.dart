d838b47839b01aebe2e80a90f393793acfb75d59
Alexey Peschany <nelex111@gmail.com>
2021-01-05 14:28:40 +0300

    Guild: remove futures

--- lib/src/sdk/guild.dart
+++ lib/src/sdk/guild.dart
@@ -1,92 +1,72 @@
 part of 'sdk.dart';
 
 class GuildData {
-  int id;
-  String host;
-  String name;
-  int owner;
-  String picture;
+  final int id;
+  final String host;
+  final String name;
+  final int owner;
+  final String avatar;
 
-  GuildData(this.id, this.host, this.name, this.owner, this.picture);
+  GuildData(this.id, this.host, this.name, this.owner, this.avatar);
 }
 
 class Guild {
-  Server _server;
+  final Server server;
+  final int id;
 
-  int _id;
-  int get id => _id;
+  Guild(this.server, this.id);
 
-  Guild(this._server, this._id) {
-    refresh();
-  }
-
-  static Future<Guild> create(Server server, String guildName) async {
-    var guild = await chat_kit.createGuild(server, guildName);
-    guild._name = Future.value(guildName);
+  static Future<Guild> create(Server server, String name) async {
+    var guild = await chat_kit.createGuild(server, name);
+    guild._name = name;
     return guild;
   }
 
-  Future<Channel> createChannel(String name, [bool isCategory = false]) => chat_kit
-      .createChannel(_server, _id, name, isCategory)
-      .then((value) => Channel(_server, _id, value, name, isCategory));
+  List<Invite> _invites;
+  List<Invite> get invites => _invites;
 
-  Future<Invite> createInvite(String name, [int uses = -1]) => chat_kit
-      .createInvite(_server, _id, name, uses)
-      .then((value) => Invite(_server, _id, value.id, value.usesCount));
+  List<Channel> _channels;
+  List<Channel> get channels => _channels;
 
-  void refresh() async {
-    var data = chat_kit.getGuild(_server, _id);
-    _name = Future(() async {
-      var doneData = await data;
-      return doneData.name;
-    });
-    _picture = Future(() async {
-      var doneData = await data;
-      return doneData.picture;
-    });
-    _owner = Future(() async {
-      var doneData = await data;
-      return User(_server, doneData.owner);
-    });
-    _members = Future(() async {
-      return await chat_kit.guildMemberList(_server, _id);
-    });
-    _channels = Future(() async {
-      return await chat_kit.channelList(_server, _id);
-    });
-    _invites = Future(() async {
-      return await chat_kit.listInvites(_server, _id);
-    });
-  }
+  List<User> _members;
+  List<User> get members => _members;
 
-  Future<List<Invite>> _invites;
-  Future<List<Invite>> get invites => _invites;
+  User _owner;
+  User get owner => _owner;
 
-  Future<List<Channel>> _channels;
-  Future<List<Channel>> get channels => _channels;
-
-  Future<List<User>> _members;
-  Future<List<User>> get members => _members;
-
-  Future<User> _owner;
-  Future<User> get owner => _owner;
-
-  Future<String> _name;
-  Future<String> get name => _name;
+  String _name;
+  String get name => _name;
 
   Future<void> setName(String name) async {
-    await chat_kit.setGuildName(_server, _id, name);
-    _name = Future.value(name);
-    return;
+    await chat_kit.setGuildName(server, id, name);
+    _name = name;
   }
 
-  Server get server => _server;
+  String _avatar;
+  String get avatar => _avatar;
 
-  Future<String> _picture;
-  Future<String> get picture => _picture;
+  Future<void> refresh() async {
+    GuildData data = await chat_kit.getGuild(server, id);
+    _name = data.name;
+    _avatar = data.avatar;
+    _owner = User(server, data.owner);
+    _members = await chat_kit.guildMemberList(server, id);
+    _channels = await chat_kit.channelList(server, id);
+    ;
+    _invites = await chat_kit.listInvites(server, id);
+    ;
+  }
 
-  Future<void> delete() => chat_kit.deleteGuild(_server, _id);
+  Future<Channel> createChannel(String name, [bool isCategory = false]) => chat_kit
+      .createChannel(server, id, name, isCategory)
+      .then((value) => Channel(server, id, value, name, isCategory));
 
-  Future<List<Channel>> listChannels() => chat_kit.channelList(_server, _id);
-  Future<List<User>> listMembers() => chat_kit.guildMemberList(_server, _id);
+  Future<Invite> createInvite(String name, [int uses = -1]) => chat_kit
+      .createInvite(server, id, name, uses)
+      .then((value) => Invite(server, value.id, id, value.usesCount));
+
+  Future<void> delete() => chat_kit.deleteGuild(server, id);
+
+  Future<List<Channel>> listChannels() => chat_kit.channelList(server, id);
+  Future<List<User>> listMembers() => chat_kit.guildMemberList(server, id);
 }
